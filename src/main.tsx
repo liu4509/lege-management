f5c722ef52e0c15efc1aaaa850c56e592805864f
zhibin <2608606700@qq.com>
2023-09-17 23:53:47 +0800

    样式引进

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// 导入清除全部样式库 初始化放在前面
+import "reset-css";
+// UI 框架样式
+// 全局样式
+import "./assets/styles/global.scss";
+// 组件样式 样式顺序很重要 涉及样式覆盖
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
